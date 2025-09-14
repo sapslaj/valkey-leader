e0477efad87e02947e71067f123a7fb3c1256156
Justin Roberson <1909600+sapslaj@users.noreply.github.com>
2025-09-14 18:26:39 -0400

    yeetify build (#2)
    * yeetify build
    
    * more yeetification
    
    * docker buildx is wack
    
    * docker buildx does not want to be yeetified
    
    * ok maybe i'm just bad at this
    
    * yep i am bad at this

---
+++ yeetfile.js
@@ -0,0 +1,31 @@
+const image = "ghcr.io/sapslaj/valkey-leader";
+
+const dockerPlatforms = [
+  "linux/amd64",
+  "linux/arm64",
+];
+
+const dockerTags = yeet.getenv("DOCKER_TAGS").split(",");
+
+const dockerBuildTags = [
+  `${image}:latest`,
+];
+for (const dockerTag of dockerTags) {
+  if (dockerTag === "" || dockerTag === "latest") {
+    continue;
+  }
+  dockerBuildTags.push(`${image}:${dockerTag}`);
+}
+
+const dockerPush = yeet.getenv("DOCKER_PUSH");
+
+yeet.run(
+  "docker",
+  "buildx",
+  "build",
+  "--platform",
+  dockerPlatforms.join(","),
+  ...(dockerBuildTags.flatMap((tag) => ["--tag", tag])),
+  ...(dockerPush === "true" ? ["--push"] : []),
+  ".",
+);
